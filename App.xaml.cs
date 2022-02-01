e7f5448e99424abad66b3c0e74f7883ae60f0993
micha mora <michamora4@gmail.com>
2022-02-01 00:06:12 -0400

    Cree la Exception

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,11 @@ namespace Parcial1
     /// </summary>
     public partial class App : Application
     {
+    private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs args)
+        {
+            MessageBox.Show($"Ocurrio un error:( {args.Exception.Message}","Error no controlado", MessageBoxButton.OK, MessageBoxImage.Error);
+            args.Handled = true;
+        }
+ 
     }
 }
