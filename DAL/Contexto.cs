430a157b8eed658ffb3805479d3fd16ddd2d2e22
micha mora <michamora4@gmail.com>
2022-02-07 18:33:28 -0400

    Hice el contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,12 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using Parcial1.Entidades;
 
    
 public class Contexto:DbContext
 {
 
-
+    public DbSet<Productos>? Productos { get; set;}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        
+        optionsBuilder.UseSqlite("Data Source = Productos.db");
     }
 }
